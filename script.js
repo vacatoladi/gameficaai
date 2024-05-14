9b1e949d79ae988073c9863ccc3417bedad12b86
vacatoladi <keltondbv0@gmail.com>
2024-05-14 17:25:32 -0300

    feat: carrossel em progresso

--- script.js
+++ script.js
@@ -23,3 +23,39 @@ window.onresize = () => {
     iconex.style.display = "inline"
     iconebarras.style.display = "none"
 }
+
+let slides = [
+    'primeiro-banner',
+    "segundo-banner",
+    'terceiro-banner'
+]
+
+let slideAtual = 0
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximoSlide = () => {
+    banner.classList.remove(slides[slideAtual])
+
+    if(slideAtual < 2){
+        slideAtual++
+    } else {
+        slideAtual = 0
+    }
+
+    banner.classList.add(slides[slideAtual])
+}
+
+const mostrarSlideAnterior = () => {
+    banner.classList.remove(slides[slideAtual])
+    
+    if(slideAtual > 0){
+        slideAtual--
+    } else {
+        slideAtual = 2
+    }
+    
+    banner.classList.add(slides[slideAtual])
+}
